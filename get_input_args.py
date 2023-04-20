4314ab4a67104cb55e56cd355af98ef13b169104
Heather Driver <83597556+HeatherDriver@users.noreply.github.com>
2023-04-20 13:33:05 +0200

    Update get_input_args.py

--- get_input_args.py
+++ get_input_args.py
@@ -17,30 +17,25 @@
 # Imports python modules
 import argparse
 
-# Define get_input_args function below please be certain to replace None
-# in the return statement with parser.parse_args() parsed argument 
-# collection that you created with this function
-# 
 def get_input_args():
     """
-    Retrieves and parses the 3 command line arguments provided by the user when
+    Retrieves and parses the 3 command line arguments provided when
     they run the program from a terminal window. This function uses Python's 
-    argparse module to created and defined these 3 command line arguments. If 
-    the user fails to provide some or all of the 3 arguments, then the default 
-    values are used for the missing arguments. 
+    argparse module and if the user fails to provide some or all of the 3 arguments, 
+    then the default values are used for the missing arguments. 
     Command Line Arguments:
       1. Image Folder as --dir with default value 'pet_images'
       2. CNN Model Architecture as --arch with default value 'vgg'
       3. Text File with Dog Names as --dogfile with default value 'dognames.txt'
     This function returns these arguments as an ArgumentParser object.
     Parameters:
-     None - simply using argparse module to create & store command line arguments
+     None 
     Returns:
      parse_args() -data structure that stores the command line arguments object  
     """
-    # Create Parse using ArgumentParser
+    # Creates Parser using ArgumentParser
     parser = argparse.ArgumentParser()
-    # Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
+    # Creates 3 command line arguments as mentioned above using add_argument() from ArguementParser method
     # Argument 1: Image Folder
     parser.add_argument('--dir', type = str, default = 'pet_images/', help = 'Path to the folder of pet images') 
     # Argument 2: CNN Model Architecture
@@ -48,6 +43,4 @@ def get_input_args():
     # Argument 3: Text file with dog names
     parser.add_argument('--dogfile', type = str, default = 'dognames.txt', help = 'Text File with Dog Names') 
     
-    # Replace None with parser.parse_args() parsed argument collection that 
-    # was created with this function 
     return parser.parse_args()
